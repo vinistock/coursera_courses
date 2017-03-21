5ced30a33b4b9562e7daf4bd1c8fec294d23cc90
Vinicius Stock <vinicius.stock@outlook.com>
2017-03-20 21:05:51 -0300

    Create image edit form

--- app/assets/javascripts/spa/subjects/images/images.component.js
+++ app/assets/javascripts/spa/subjects/images/images.component.js
@@ -1,14 +1,24 @@
 (function () {
     "use strict";
 
-    angular.module("spa.subjects").component("sdImageSelector", {
-       templateUrl: imageSelectorTemplateUrl,
-        controller: ImageSelectorController
-    });
+    angular.module("spa.subjects")
+        .component("sdImageSelector", {
+            templateUrl: imageSelectorTemplateUrl,
+            controller: ImageSelectorController
+        })
+        .component("sdImageEditor", {
+            templateUrl: imageEditorTemplateUrl,
+            controller: ImageEditorController
+        });
 
     imageSelectorTemplateUrl.$inject = ["spa.config.APP_CONFIG"];
     function imageSelectorTemplateUrl (APP_CONFIG) {
         return APP_CONFIG.image_selector_html;
+    }
+
+    imageEditorTemplateUrl.$inject = ["spa.config.APP_CONFIG"];
+    function imageEditorTemplateUrl (APP_CONFIG) {
+        return APP_CONFIG.image_editor_html;
     }
 
     ImageSelectorController.$inject = ["$scope", "spa.subjects.Image", "$stateParams"];
@@ -22,5 +32,25 @@
                 vm.items = Image.query();
             }
         };
+    }
+
+    ImageEditorController.$inject = ["$scope", "spa.subjects.Image", "$stateParams"];
+    function ImageEditorController ($scope, Image, $stateParams) {
+        var vm = this;
+
+        vm.$onInit = function () {
+            console.log("ImageEditorController", $scope);
+
+            if($stateParams.id) {
+                vm.item = Image.get({id: $stateParams.id});
+            } else {
+                newResource();
+            }
+        };
+
+        function newResource () {
+            vm.item = new Image();
+            return vm.item;
+        }
     }
 })();
