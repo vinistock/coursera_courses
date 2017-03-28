cb10f777bc316e9766f8da754a9dfc80688a91a1
Vinicius Stock <vinicius.stock@outlook.com>
2017-03-27 21:26:48 -0300

    Update whoami

--- app/models/role.rb
+++ app/models/role.rb
@@ -9,4 +9,6 @@ class Role < ActiveRecord::Base
   scope :relevant, -> (model_name, model_id) do
     where('mname is null or (mname=:mname and (mid is null or mid=:mid))', mname: model_name, mid: model_id)
   end
+
+  scope :application, -> { where('mid is null') }
 end
