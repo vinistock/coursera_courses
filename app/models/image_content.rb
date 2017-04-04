bb796ac117ac9742d2c283fce078b872aa09129f
Vinicius Stock <vinicius.stock@outlook.com>
2017-04-04 19:54:10 -0300

    Advance image content

--- app/models/image_content.rb
+++ app/models/image_content.rb
@@ -1,5 +1,6 @@
 class ImageContent
   CONTENT_TYPES = %w(image/jpeg image/jpg)
+  MAX_CONTENT_SIZE = 10*1000*1024
 
   include Mongoid::Document
   field :image_id, type: Integer
@@ -8,6 +9,24 @@ class ImageContent
   field :content_type, type: String
   field :content, type: BSON::Binary
   field :original, type: Mongoid::Boolean
+
+  validates_presence_of :image_id, :height, :width, :content_type, :content
+  validate :validate_width_height
+  validate :validate_content_length
+
+  def validate_width_height
+    if (!width || !height) && content
+      unless CONTENT_TYPES.include?(content_type)
+        errors.add(:content_type, "[#{content_type}] not supported type #{CONTENT_TYPES}")
+      end
+    end
+  end
+
+  def validate_content_length
+    if content && content.data.size > MAX_CONTENT_SIZE
+      errors.add(:content, "#{content.data.size} too large, greater than max #{MAX_CONTENT_SIZE}")
+    end
+  end
 
   def content=(value)
     if self[:content]
--- spec/factories/images.rb
+++ spec/factories/images.rb
@@ -2,6 +2,11 @@ FactoryGirl.define do
   factory :image do
     sequence(:caption) { |n| n%2==0 ? nil : Faker::Lorem.sentence(1).chomp('.') }
     creator_id 1
+    image_content { attributes_for(:image_content) }
+
+    after(:build) do |image|
+      image.image_content = build(:image_content, image.image_content) if image.image_content
+    end
 
     trait :with_caption do
       caption { Faker::Lorem.sentence(1).chomp('.') }
